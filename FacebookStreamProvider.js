41ccf3e22c72739b2aa9ff998877bf051856d1e8
Dan Palmer <dan.palmer@me.com>
2012-07-18 23:07:23 +0100

    plugin now creates itself, can return authentication options

--- FacebookStreamProvider.js
+++ FacebookStreamProvider.js
@@ -1,1 +1,23 @@
-"This is some test content";
+(function(R) {
+	var FacebookStreamProvider = function() {
+		
+	};
+
+	FacebookStreamProvider.prototype.authRequirements = function() {
+		return {
+			authType: "credentials",
+			field1: {
+					"name": "Email",
+					"type": "email",
+					"identifier": "email"
+			},
+			field2:	{
+				"name": "Password",
+				"type": "secure",
+				"identifier": "password"
+			}
+		};
+	};
+
+	return new FacebookStreamProvider();
+})
